979a3efdc3c0736f4dab1d4c6db0871fa320baaa
Therion <vinhquangdoan0@gmail.com>
2026-03-17 18:30:06 +0700

    FileManager

---
+++ src/Utilities/FileManager.java
@@ -0,0 +1,41 @@
+
+package Utilities;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    public static List<String> readFile(String fileName) {
+        List<String> lines = new ArrayList<>();
+        File file = new File(fileName);
+        
+        if (!file.exists()) return lines;
+        
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream(file), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    lines.add(line);
+                }
+            }
+        }
+        catch (IOException e) {
+            System.err.println("Error reading file " + fileName + ": " + e.getMessage());
+        }
+        return lines;
+    }
+    
+    public static void writeFile(String fileName, List<String> data) {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileName), "UTF-8"))) {
+            for (String line : data) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing to file " + fileName + ": " + e.getMessage());
+        }
+    }
+}
